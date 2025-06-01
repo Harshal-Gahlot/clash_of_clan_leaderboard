b6a2a7ebed625ba205ef9f57be953b59ffc80715
Harshal <gahlotharshal@gmail.com>
2025-06-01 20:53:20 +0530

    made 2nd endpoint but can't check it as the event currentCWL havn't started for my clan with which i was test for like an hour, took me a while to realise why it rturn not found, peek programming 🫠

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const axios = require('axios');
 // const cors = require('cors')
 
-const listClanMembersRouter = require('./router/clan/listClanMembers')
+const clanRouter = require('./router/clanRouter')
 
 const app = express();
 const PORT = 3000;    
 // app.use(cors()); 
 
-app.use("/api", listClanMembersRouter)
+app.use("/api/clan", clanRouter)
 
 // DEV: just to copy the ip and make new API key every time ip changes, that's how it works sadly.
 axios.get('https://api64.ipify.org?format=json').then(
---
+++ server/router/clanRouter.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const { Router } = require('express');
+
+const clanRouter = Router();
+clanRouter.use(express.json());
+const token = process.env.CLASH_API;
+// console.log("Token:", token); 
+
+function parsTagMiddleware(req, res, next) {
+    let Tag = req.params.clanTag; // get the clan tag from the request
+    if (!Tag) {
+        return res.status(404).json({ error: 'Clan tag is required' });
+    }
+    console.log("Tag:", Tag);
+    // encode the clan tag from the request i.e. makes # -> %23
+    if (Tag.startsWith('#')) {
+        Tag = encodeURIComponent(Tag);
+    } else {
+        Tag = encodeURIComponent(`#${Tag}`);
+    }
+
+    req.params.clanTag = Tag;
+    next();
+}
+
+clanRouter.get('/clan-members/:clanTag', parsTagMiddleware, async (req, res) => {
+    let clanTag = req.params.clanTag;
+
+    try {
+        console.log("before try");
+        const response = await axios.get(`https://api.clashofclans.com/v1/clans/${clanTag}/members`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log("after try");
+        const members = response.data.items;
+        res.json(members); // send to frontend
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.json(error);
+    }
+});
+
+clanRouter.get("/:clanTag/currentWar/leaguegroup", parsTagMiddleware, async (req, res) => {
+    let clanTag = req.params.clanTag;
+
+    try {
+        response = await axios.get(`https://api.clashofclans.com/v1/clans/${clanTag}/currentwar/leaguegroup`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        const data = response.data;
+        res.json(data);
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.json(error);
+    }
+});
+
+module.exports = clanRouter; 
